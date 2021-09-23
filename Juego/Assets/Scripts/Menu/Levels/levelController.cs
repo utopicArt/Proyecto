478b854ad0e98ae63e46a80cdadfd5218f805778
Adrian-Marin <a18310176@live.ceti.mx>
2021-09-23 15:27:18 -0500

    Menu base del juego
    Se añadió la interfaz gráfica de los menus de configuración y selección de nivel.
    
    Esta es la base del juego por lo que los siguientes cambios deberían realizarse en branchs diferentes hasta que sean estables las nuevas versiones.

---
+++ Juego/Assets/Scripts/Menu/Levels/levelController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+/*
+    Archivo de controlador de levels y dificultad
+*/
+
+public class levelController : MonoBehaviour
+{
+    private int gameLevel = 0;
+    private Color c;
+    public GameObject message;
+
+    public void setLevel(int level)
+    {
+        GameObject[] levels;
+        levels = GameObject.FindGameObjectsWithTag("Nivel");
+        gameLevel = level;
+        foreach (GameObject btn in levels)
+        {
+            c = btn.GetComponent<Image>().color;
+            c.a = 0f;
+            btn.GetComponent<Image>().color = c;
+        }
+        c = levels[level - 1].GetComponent<Image>().color;
+        c.a = 0.29f;
+        levels[level - 1].GetComponent<Image>().color = c;
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void comprobarValores()
+    {
+        int dificultad = PlayerPrefs.GetInt("difficulty", 2);
+        if (gameLevel > 0){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else{
+            message.SetActive(true);
+        }
+    }
+}
